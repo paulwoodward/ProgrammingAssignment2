90f7fcbb8d8ae14b644597bf63b9ce6e7ea0c7eb
paulwoodward <paul.woodward@gmail.com>
2014-06-22 23:10:17 +0100

    Coursework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+# Creates a wrapper for a matrix and verifies that it is square
+# - return: A wrapper for the matrix x
 makeCacheMatrix <- function(x = matrix()) {
-
+    x <- ensureSquareMatrix(x)
+    i <- NULL
+    
+    set <- function(y) {
+        x <<- ensureSquareMatrix(y)
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    setInverse <- function(inv) i <<- inv
+    getInverse <- function() i
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+# Validates that x is a square matrix
+# - return: x
+ensureSquareMatrix <- function (x) {
+    if(!is.matrix(x)) {
+        stop("x must be a matrix")
+    }
+    
+    if(dim(x)[1] != dim(x)[2]) {
+        stop("Cache matrix requires a square matrix")
+    }
+    x
+}
 
-## Write a short comment describing this function
-
+# Gets and caches the inverse of the given matrix
+# - return: The inverse of x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("Returning cached inverse matrix")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
